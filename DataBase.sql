fc70e11b7a528aca7bd91fbe763d341d76360aff
Ярослав Алексеев <mmmoonnn@mail.ru>
2020-07-16 21:27:58 +0300

    Sql-скрипт для развертывания базы данных

---
+++ DataBase.sql
@@ -0,0 +1,25 @@
+﻿CREATE DATABASE `WeatherDatabase`;
+
+USE `WeatherDatabase`;
+
+DROP TABLE IF EXISTS `cities`;
+CREATE TABLE `Cities` (
+    `Id` int NOT NULL AUTO_INCREMENT,
+    `Name` text NULL,
+    PRIMARY KEY (`Id`)
+);
+
+DROP TABLE IF EXISTS `weathers`;
+CREATE TABLE `Weathers` (
+`Id` int NOT NULL AUTO_INCREMENT,
+  `Date` datetime NOT NULL,
+  `State` text,
+  `Temperature` float NOT NULL,
+  `WindSpeed` float NOT NULL,
+  `ChancePrecipitation` int NOT NULL,
+  `AirHumidity` int NOT NULL,
+  `CityId` int NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `IX_Weathers_CityId` (`CityId`),
+  CONSTRAINT `FK_Weathers_Cities_CityId` FOREIGN KEY (`CityId`) REFERENCES `cities` (`Id`) ON DELETE RESTRICT
+);
